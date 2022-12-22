467ebd890a596e3685c90a2ed8c510b642faf238
Arturo Soberanes <arturo.soberanes01@gmail.com>
2022-12-22 01:30:44 -0600

    Test with Octokit for APIRest of Github

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import { Container } from "react-bootstrap";
+import { Octokit } from "octokit";
+
 function App() {
+  const octokit = new Octokit({ });
+  async function getDataAPI() {
+    const res = await octokit.request("GET /octocat", {});
+    console.log(res)
+  }
+  getDataAPI()
   return (
     <Container>
       <h1>Hello World!</h1>
