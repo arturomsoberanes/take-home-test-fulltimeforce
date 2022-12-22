5dd4f1d43c9265581bb4b246da6623b1228e0ff0
Arturo Soberanes <arturo.soberanes01@gmail.com>
2022-12-22 02:43:37 -0600

    Add logic for request to API the commits of repository and render components with that data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import { Container } from "react-bootstrap";
 import { Octokit } from "octokit";
+import { useState, useEffect } from "react";
+import ListOfCommits from "./components/ListOfCommits";
+import './App.css'
 
 function App() {
-  const octokit = new Octokit({ });
-  async function getDataAPI() {
-    const res = await octokit.request("GET /octocat", {});
-    console.log(res)
-  }
-  getDataAPI()
+  const [commits, setCommits] = useState(null)
+
+  useEffect(() => {
+    const octokit = new Octokit({ });
+    async function getDataAPI() {
+      const res = await octokit.request('GET /repos/{owner}/{repo}/commits{?sha,path,author,since,until,per_page,page}', {
+        owner: 'arturomsoberanes',
+        repo: 'take-home-test-fulltimeforce'
+      })
+      return res.data
+    }
+
+    getDataAPI().then(data => {
+      console.log(data)
+      setCommits(data)
+    })
+
+
+  }, [])
   return (
-    <Container>
-      <h1>Hello World!</h1>
+    <Container className="text-center">
+      <h1 className="my-4">Latest Commits of this project</h1>
+      <ul>
+        { 
+          commits ? (
+            <ListOfCommits commits={commits}/>
+          ) : <h1>Loading...</h1>
+        }
+      </ul>
     </Container>
   )
 }
